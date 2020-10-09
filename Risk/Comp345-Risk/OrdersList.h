aeba2a3f799ad3e84fe555b75b4d63d294d92c38
Joy Thomas <leaderjoy@hotmail.com>
2020-10-08 21:18:15 -0400

    Add OrderList implementation

--- Risk/Comp345-Risk/OrdersList.h
+++ Risk/Comp345-Risk/OrdersList.h
@@ -1,5 +1,93 @@
 #pragma once
 
-namespace OrdersList {
+
+#include <string>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Order {
+public:
+	bool* ex = new bool();  //This boolean is used to determine if an order has been executed or not
+	Order();
+	virtual ~Order();
+	Order(const Order& a);
+	virtual void execute(); //added this virtual method, so that the subclasses can override it
+	virtual string getName() const;  // getName is used for the << operator to display the description of the order
+	friend std::ostream& operator <<(std::ostream& stream, const Order& e);
+	Order& operator = (const Order& a);
+};
+
+class Deploy : public Order {
+public:
+
+	virtual string getName() const;
+	bool validate();
+	void execute();
+	Deploy();                     //default constructor
+	Deploy(const Deploy& d);      // copy constructor
+	Deploy& operator=(const Deploy& d);  //assignment operator
+};
+
+class Advance : public Order {
+public:
+	virtual string getName() const;
+	bool validate();
+	void execute();
+	Advance();
+	Advance(const Advance& d);
+	Advance& operator = (const Advance& a);
+};
+class Bomb : public Order {
+public:
+	virtual string getName() const;
+	bool validate();
+	void execute();
+	Bomb();
+	Bomb(const Bomb& d);
+	Bomb& operator = (const Bomb& a);
+};
+class Airlift : public Order {
+public:
+	virtual string getName() const;
+	bool validate();
+	void execute();
+	Airlift();
+	Airlift(const Airlift& d);
+	Airlift& operator = (const Airlift& a);
+};
+class Blockade : public Order {
+public:
+	virtual string getName() const;
+	bool validate();
+	void execute();
+	Blockade();
+	Blockade(const Blockade& d);
+	Blockade& operator = (const Blockade& a);
+};
+class Negotiate : public Order {
+public:
+	virtual string getName() const;
+	bool validate();
+	void execute();
+	Negotiate();
+	Negotiate(const Negotiate& d);
+	Negotiate& operator = (const Negotiate& a);
+};
+
+class OrderList {
+public:
+	std::vector<Order*> list;   //pointer to the list of Orders
+	OrderList();                 //defaut constructor
+	OrderList(const OrderList& d);
+	OrderList& operator =(const OrderList& e);
+	~OrderList();                // destructor
+
+
+	void remove(int ind);
+	void move(int ind, int final);
+
+};
+namespace OrdersListDriver {
 	int main();
 }
