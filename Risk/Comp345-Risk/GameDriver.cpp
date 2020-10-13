2c2231fd9de0a356c3776cd3db1ef44357124a75
Nicholas Apanian <nicholas.apanian@outlook.com>
2020-10-12 21:27:48 -0400

    fix small bugs
    include pointers in the driver class and delete the pointers we create

--- Risk/Comp345-Risk/GameDriver.cpp
+++ Risk/Comp345-Risk/GameDriver.cpp
@@ -7,13 +7,13 @@
 //Uncomment any of the main function calls to run that part of the project.
 int main() {
 	//Part 1
-	Map::main();
+	//Map::main();
 	//Part 2
-	MapLoader::main();
+	//MapLoader::main();
 	//Part 3
-	Player::main();
+	//Player::main();
 	//Part 4
 	OrdersDriver::main();
 	//Part 5
-	Cards::main();
+	//Cards::main();
 }
