9ee4ba57d447b5a99dfba800d80a423eb6d6c0e6
Antoine <antoine.beiten@gmail.com>
2020-11-12 17:57:22 -0500

    Part 1 of GameEngine - Also corrected a Player constructor.

--- Risk/Comp345-Risk/GameEngine.cpp
+++ Risk/Comp345-Risk/GameEngine.cpp
@@ -1,68 +1,120 @@
-#include "Map.h"
-#include "MapLoader.h"
-#include "Player.h"
-#include "Orders.h"
-#include "Cards.h"
 #include "GameEngine.h"
 #include <iostream>
 
-Map* verifyMap(string mapPath);
 
-int main() {
+bool observerOn{ 0 };
 
-	int selectedMap;
-	int numOfPlayers;
-	bool observerOn{ false };
-	bool validMap{ false };
-	std::string path = "Maps";
-	std::string Maps[7]{ 
-		"artic.map", "canada.map",
-		"europe.map", "solar.map",
-		"sw_baltic.map", "invalidMapTest.map",
-		"invalidSolarDisconnected.map" };
+	int main() {
 
-	std::cout << "GameEngine Driver Starting...\n" << std::endl;
-	
-	Map* map = NULL;
-
-	while (map == NULL && !validMap) {
-
-		std::cout << "Please select a map by entering its number from the following list:\n" << std::endl;
-
-		for (size_t i = 0; i < 7; i++)
-		{
-			std::cout << i << ": " << Maps[i] << std::endl;
-		}
-
-		std::cin >> selectedMap;
-		std::cout << "You've selected map number " << selectedMap << "." << endl;
-		std::cout << "Verifying validity of map file..." << endl;
-
+		int selectedMap;
+		Map* map = NULL;
 		MapLoader m;
-		map = m.loadMap("Maps\\" + Maps[selectedMap]);
+		int numOfPlayers{ 0 };
 
-		//map = verifyMap();
+		bool validMap{ false };
 
-		if (map != NULL) {
-			cout << "Map file is valid. Verifying if it is a connected graph..." << endl;
-			validMap = map->validate();
+		std::vector<Player*> players;
+		Cards* card = new Cards();
+		Deck* deck = new Deck();
+
+		std::string path = "Maps";
+		std::string Maps[7]{
+			"artic.map", "canada.map",
+			"europe.map", "solar.map",
+			"sw_baltic.map", "invalidMapTest.map",
+			"invalidSolarDisconnected.map" };
+
+		std::cout << "--- GameEngine Driver Starting... ---\n" << std::endl;
+
+		while (map == NULL && !validMap) {
+
+			std::cout << "Please select a map by entering its number from the following list:\n" << std::endl;
+
+			for (size_t i = 0; i < 7; i++)
+			{
+				std::cout << i << ": " << Maps[i] << std::endl;
+			}
+
+			std::cin >> selectedMap;
+			std::cout << "--- You've selected map number " << selectedMap << ". ---" << endl;
+			std::cout << "--- Verifying validity of map file... ---" << endl;
+
+			map = m.loadMap("Maps\\" + Maps[selectedMap]);
+
+			if (map != NULL) {
+				cout << "--- Map file is valid. Verifying if it is a connected graph... ---" << endl;
+				validMap = map->validate();
+			}
 		}
+
+		cout << "--- Congratulations, the map is valid! ---" << endl;
+
+		while (numOfPlayers < 2 || numOfPlayers >5) {
+
+			cout << "Please enter the number of players (2-5): " << endl;
+			cin >> numOfPlayers;
+
+			if (cin.fail()) {
+				cin.clear();
+				cin.ignore(512, '\n');
+				cout << "You did not enter a valid integer." << endl;
+			}
+
+			if (numOfPlayers < 2 || numOfPlayers > 5) {
+				cout << "Invalid number of players. Please try a number between 2 and 5." << endl;
+			}
+		}
+
+		for (size_t i = 0; i < numOfPlayers; i++)
+		{
+			std::string playerName{ "Player " + std::to_string(i) };
+			Player* player = new Player(playerName);
+			players.push_back(player);
+		}
+
+		cout << "You have chosen " << numOfPlayers << " players." << endl;
+
+		for (size_t i = 0; i < players.size(); i++)
+		{
+			cout << "The players are: " << endl;
+			cout << *players[i] << endl;
+		}
+
+		cout << "--- Initializing deck ---" << endl;
+		deck->initialize(*deck, *card);
+		cout << *deck << endl;
+		cout << "--- Shuffling deck ---" << endl;
+		deck->shuffle(*deck);
+		cout << *deck << endl;
+
+
+		cout << "Please choose whether you'd like the observers to be On(1) or Off(0): " << endl;
+		cin >> observerOn;
+
+		while (cin.fail()) {
+			cin.clear();
+			cin.ignore(512, '\n');
+			cout << "You did not enter a valid input. Setting the observers off." << endl;
+		}
+
+		if (observerOn) {
+			cout << "The observers are now on." << endl;
+		}
+		else {
+			cout << "The observers are off." << endl;
+		}
+
+		return 0;
 	}
 
-
-	cout << "yay valid map" << endl;
-
-	
-
-	
-
-
-}
-
-Map* verifyMap(string mapPath) {
-	MapLoader m;
-	Map* map;
-	map = m.loadMap(mapPath);
-
-	return map;
+/// <summary>
+/// Method to turn the observers on/off during the game start phase
+/// </summary>
+void turnObserversOnOff() {
+	if (observerOn) {
+		observerOn = 0;
+	}
+	else {
+		observerOn = 1;
+	}
 }
--- Risk/Comp345-Risk/GameEngine.h
+++ Risk/Comp345-Risk/GameEngine.h
@@ -1,3 +1,12 @@
 #pragma once
+#include "Map.h"
+#include "MapLoader.h"
+#include "Player.h"
+#include "Orders.h"
+#include "Cards.h"
+#include <vector>
 
 using namespace std;
+
+int main();
+void turnObserversOnOff();
