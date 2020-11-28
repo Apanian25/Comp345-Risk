36c37c1e5d0b259c39a7b3f0382116cb4849b109
Paul.Vlahopoulos@outlook.com <Paul.Vlahopoulos@outlook.com>
2020-11-28 14:36:13 -0500

    benevolent player strategy and neutral game player

--- Risk/Comp345-Risk/PlayerStrategies.cpp
+++ Risk/Comp345-Risk/PlayerStrategies.cpp
@@ -1,6 +1,7 @@
 #include "PlayerStrategies.h"
 #include "Player.h"
 #include "Orders.h"
+#include <algorithm>
 
 // *********************** HUMAN PLAYER ***********************
 vector<Territory*> HumanPlayerStrategy::toAttack(Player* player)
@@ -39,18 +40,76 @@ Order* AggressivePlayerStrategy::issueOrder(Player* player)
 
 vector<Territory*> BenevolentPlayerStrategy::toAttack(Player* player)
 {
-	return vector<Territory*>();
+	vector<Territory*> vectToAtk = player->toAttackUnSorted();
+
+
+	return vectToAtk;
 }
+
 
 vector<Territory*> BenevolentPlayerStrategy::toDefend(Player* player)
 {
-	return vector<Territory*>();
+	 vector<Territory*> vecToDef = player->getTerritories();
+
+	 std::sort(vecToDef.begin(), vecToDef.end(), [](Territory* t1, Territory* t2)
+		 {
+			 return t1->numberOfArmies < t2->numberOfArmies;
+		 });
+
+	 return vecToDef;
 }
 
 Order* BenevolentPlayerStrategy::issueOrder(Player* player)
 {
+	if (player->getNumOfArmies() > 0)
+	{
+		toDefend(player)[0];
+		Deploy* deploy = new Deploy(player, toDefend(player)[0], player->getNumOfArmies());
+		player->numOfArmies = 0;
+		player->orders.push_back(deploy);
+		return deploy;
+	}
+	else
+	{
+
+		if (player->hand->hand.size() > 0) 
+		{
+			std::cout << "this is for cards " << std :: endl;
+		}
+
+		else
+		{
+			vector<Territory*> toDef = toDefend(player);
+			Territory* weakest = toDef[0];
+			vector<Territory*> reinforcements = vector<Territory*>();
+
+			for (Territory* canReinforceWith : weakest->adjacentTerritoriesFrom)
+			{
+				if (canReinforceWith->ownedBy = player->id && canReinforceWith->numberOfArmies > 0)
+				{
+					reinforcements.push_back(canReinforceWith);
+
+				}
+				
+			}
+			if (reinforcements.size() > 0)
+			{
+				std::sort(reinforcements.begin(), reinforcements.end(), [](Territory* t1, Territory* t2)
+					{
+						return t1->numberOfArmies > t2->numberOfArmies;
+					});
+
+				Advance* advance = new Advance(player, reinforcements[0], weakest, reinforcements[0]->numberOfArmies / 2);
+				player->Notify("Added defending advance order");
+				player->orders.push_back(advance);
+				
+			}
+		}
+		
+	}
 	return nullptr;
 }
+
 
 // *********************** NEUTRAL PLAYER ***********************
 
