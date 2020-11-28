ee6adb54566aef38fee35763b137c9c996776383
Nicholas Apanian <nicholas.apanian@outlook.com>
2020-11-28 14:44:28 -0500

    Finish Aggressive Player Strategy

--- Risk/Comp345-Risk/PlayerStrategies.cpp
+++ Risk/Comp345-Risk/PlayerStrategies.cpp
@@ -1,6 +1,9 @@
 #include "PlayerStrategies.h"
 #include "Player.h"
 #include "Orders.h"
+#include <algorithm>
+
+using namespace std;
 
 // *********************** HUMAN PLAYER ***********************
 vector<Territory*> HumanPlayerStrategy::toAttack(Player* player)
@@ -22,16 +25,76 @@ Order* HumanPlayerStrategy::issueOrder(Player* player)
 
 vector<Territory*> AggressivePlayerStrategy::toAttack(Player* player)
 {
-	return vector<Territory*>();
+	vector<Territory*> vecToAtk = player->toAttackUnSorted();
+
+	std::sort(vecToAtk.begin(), vecToAtk.end(), [](Territory* t1, Territory* t2) {
+		//largest number of armies is first
+		return t1->numberOfArmies > t2->numberOfArmies;
+	});
+
+	return vecToAtk;
 }
 
 vector<Territory*> AggressivePlayerStrategy::toDefend(Player* player)
 {
-	return vector<Territory*>();
+	vector<Territory*> vecToDef = player->getTerritories();
+	std::sort(vecToDef.begin(), vecToDef.end(), [](Territory* t1, Territory* t2) {
+		//largest number of armies is first
+		return t1->numberOfArmies > t2->numberOfArmies;
+	});
+
+	return vecToDef;
 }
 
 Order* AggressivePlayerStrategy::issueOrder(Player* player)
 {
+	if (player->getNumOfArmies() > 0) {
+		//DEPLOY PHASE
+		Deploy* dep = new Deploy(player, toDefend(player)[0], player->getNumOfArmies());
+		player->orders.push_back(dep);
+		player->numOfArmies = 0;
+		return dep;
+	}
+	else {
+		//DEPLOY PHASE COMPLETED 
+		bool canPlayACard = false;
+		if (player->hand->hand.size() > 0) {
+
+			Order* order = player->hand->play(player);
+			if (order != nullptr) {
+				canPlayACard = true;
+				player->orders.push_back(order);
+				player->Notify("Added Deploy order");
+			}
+		}
+
+		if(!canPlayACard) {
+			vector<Territory*> toDef = toDefend(player);
+			Territory* strongest = toDef[0];
+			//REINFORCES STRONGEST
+			for (Territory* canReinforceWith : toDef) {
+				if (canReinforceWith == strongest)
+					continue;
+				if (std::count(strongest->adjacentTerritoriesFrom.begin(), strongest->adjacentTerritoriesFrom.end(), canReinforceWith) > 0) {
+					canReinforceWith->commitedNumberOfArmies = canReinforceWith->numberOfArmies;
+					Advance* reinf = new Advance(player, canReinforceWith, strongest, canReinforceWith->numberOfArmies);
+					player->Notify("Added defending advance order");
+					player->orders.push_back(reinf);
+				}
+			}
+			//ATTACK EVERYTHING WE CAN
+			for (Territory* territory : toAttack(player)) {
+				for (Territory* canAttackWith : territory->adjacentTerritoriesFrom) {
+					if (canAttackWith->ownedBy == player->id && canAttackWith->numberOfArmies > 0 && canAttackWith->commitedNumberOfArmies != canAttackWith->numberOfArmies) {
+						canAttackWith->commitedNumberOfArmies = canAttackWith->numberOfArmies;
+						Advance* adv = new Advance(player, canAttackWith, territory, canAttackWith->numberOfArmies);
+						player->Notify("Added attacking advance order");
+						player->orders.push_back(adv);
+					}
+				}
+			}
+		}
+	}
 	return nullptr;
 }
 
